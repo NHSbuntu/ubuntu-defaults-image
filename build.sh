650658299448beb45d0e9b6abba3d3151c757975
Rob Dyke <robdyke@gmail.com>
2017-07-12 20:19:34 +0100

    Build in subdir per arch
    build.sh creates $WORKDIR as amd64|i386

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
+#! /bin/sh
+# Wrapper for NHSbuntu customised ubuntu-defaults-image
+
+WORKDIR=$1
 export LB_ISO_TITLE="NHSbuntu"
 export LB_ISO_VOLUME="NHSbuntu xenial $(date +%Y%m%d)"
 export BUILD_ISO_FILENAME="NHSbuntu-$1-$(date +%Y%m%d)"
-./ubuntu-defaults-image --ppa nhsbuntu/ppa --package nhsbuntu-default-settings --arch $1 --release xenial --flavor $2 --repo nhsbuntu/nhsbuntu-default-settings
-mv binary.hybrid.iso $BUILD_ISO_FILENAME-binary.iso
-mv livecd.ubuntu-gnome.iso $BUILD_ISO_FILENAME-livecd.iso
+
+mkdir $WORKDIR
+cd $WORKDIR
+../ubuntu-defaults-image --ppa nhsbuntu/ppa --package nhsbuntu-default-settings --arch $1 --release xenial --flavor ubuntu-gnome --repo nhsbuntu/nhsbuntu-default-settings
+mv binary.hybrid.iso ../$BUILD_ISO_FILENAME-binary.iso
+mv livecd.ubuntu-gnome.iso ../$BUILD_ISO_FILENAME-livecd.iso
+cd ../
+rm -rf $WORKDIR
