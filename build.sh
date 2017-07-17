cea0b8a23f0fc82d7352306a0fcca3c2af31e9a7
Rob Dyke <robdyke@gmail.com>
2017-07-17 23:40:04 +0100

    Require ENV VAR for BUILDARCH
    Usage example
    $ BUILDARCH=amd64 ./build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,20 @@
 #! /bin/sh
 # Wrapper for NHSbuntu customised ubuntu-defaults-image
+# Expects ENV VAR for arch
 
-WORKDIR=$1
+# Usage
+# $ BUILDARCH=amd64 ./build.sh
+
+WORKDIR=$BUILDARCH
 export LB_ISO_TITLE="NHSbuntu"
 export LB_ISO_VOLUME="NHSbuntu xenial $(date +%Y%m%d)"
-export BUILD_ISO_FILENAME="NHSbuntu-$1-$(date +%Y%m%d)"
+export BUILD_ISO_FILENAME="NHSbuntu-$BUILDARCH-$(date +%Y%m%d)"
 
 apt-get install -y chroot curl git live-build cdebootstrap ubuntu-defaults-builder syslinux-utils genisoimage memtest86+ syslinux syslinux-themes-ubuntu-xenial gfxboot-theme-ubuntu livecd-rootfs
 
 mkdir $WORKDIR
 cd $WORKDIR
-../ubuntu-defaults-image --ppa nhsbuntu/ppa --package nhsbuntu-default-settings --arch $1 --release xenial --flavor ubuntu-gnome --repo nhsbuntu/nhsbuntu-default-settings
+../ubuntu-defaults-image --ppa nhsbuntu/ppa --package nhsbuntu-default-settings --arch $BUILDARCH --release xenial --flavor ubuntu-gnome --repo nhsbuntu/nhsbuntu-default-settings
 mv binary.hybrid.iso ../$BUILD_ISO_FILENAME-binary.iso
 mv livecd.ubuntu-gnome.iso ../$BUILD_ISO_FILENAME-livecd.iso
 cd ../
