b191740c9241ef270a0e3862dde2bb14080600f8
Rob Dyke <robdyke@gmail.com>
2017-07-19 02:00:38 +0100

    Add patch of /usr/lib/live/build/lb_binary_disk Add INFO: messages to console Update VARS

--- build.sh
+++ build.sh
@@ -5,17 +5,35 @@
 # Usage
 # $ BUILDARCH=amd64 ./build.sh
 
-WORKDIR=$BUILDARCH
-export LB_ISO_TITLE="NHSbuntu"
+export BUILD_ISO_ARCH=$BUILDARCH
+export BUILD_ISO_WORKDIR=$BUILDARCH
+export BUILD_ISO_FILE="NHSbuntu-$BUILD_ISO_ARCH-$(date +%Y%m%d)"
+export LB_ISO_TITLE=NHSbuntu
 export LB_ISO_VOLUME="NHSbuntu xenial $(date +%Y%m%d)"
-export BUILD_ISO_FILENAME="NHSbuntu-$BUILDARCH-$(date +%Y%m%d)"
 
-apt-get install -y chroot curl git live-build cdebootstrap ubuntu-defaults-builder syslinux-utils genisoimage memtest86+ syslinux syslinux-themes-ubuntu-xenial gfxboot-theme-ubuntu livecd-rootfs
+echo "INFO: Build architecture is ${BUILD_ISO_ARCH}"
+echo "INFO: Build workdir is ${BUILD_ISO_WORKDIR}"
+echo "INFO: Build ISO filename is ${BUILD_ISO_FILE}"
+echo "INFO: live-build ISO title is ${LB_ISO_TITLE}"
+echo "INFO: live-build ISO volume is ${LB_ISO_VOLUME}"
 
-mkdir $WORKDIR
-cd $WORKDIR
-../ubuntu-defaults-image --ppa nhsbuntu/ppa --package nhsbuntu-default-settings --arch $BUILDARCH --release xenial --flavor ubuntu-gnome --repo nhsbuntu/nhsbuntu-default-settings
-mv binary.hybrid.iso ../$BUILD_ISO_FILENAME-binary.iso
-mv livecd.ubuntu-gnome.iso ../$BUILD_ISO_FILENAME-livecd.iso
+apt-get install -qq -y chroot curl git live-build cdebootstrap ubuntu-defaults-builder syslinux-utils genisoimage memtest86+ syslinux syslinux-themes-ubuntu-xenial gfxboot-theme-ubuntu livecd-rootfs
+
+# Patch lb_binary_disk to support $LB_ISO_VOLUME
+echo "INFO: Patching lb_binary_disk"
+cp /usr/lib/live/build/lb_binary_disk /usr/lib/live/build/lb_binary_disk.orig
+sed -i 's/TITLE="Ubuntu"/TITLE="${LB_ISO_TITLE}"/' /usr/lib/live/build/lb_binary_disk
+#diff /usr/lib/live/build/lb_binary_disk /usr/lib/live/build/lb_binary_disk.orig
+echo "INFO: Patched lb_binary_disk"
+
+# Do the build
+echo "INFO: Running build"
+mkdir $BUILD_ISO_WORKDIR
+cd $BUILD_ISO_WORKDIR
+../ubuntu-defaults-image --ppa nhsbuntu/ppa --package nhsbuntu-default-settings --arch $BUILD_ISO_ARCH --release xenial --flavor ubuntu-gnome --repo nhsbuntu/nhsbuntu-default-settings > ../$BUILD_ISO_ARCH.log 2>&1
+#../ubuntu-defaults-image --ppa nhsbuntu/ppa --package nhsbuntu-default-settings --arch $BUILD_ISO_ARCH --release xenial --flavor ubuntu-gnome --repo nhsbuntu/nhsbuntu-default-settings
+echo "INFO: Moving ISO files"
+mv binary.hybrid.iso ../$BUILD_ISO_FILE-binary.iso
+mv livecd.ubuntu-gnome.iso ../$BUILD_ISO_FILE-livecd.iso
 cd ../
-rm -rf $WORKDIR
+rm -rf $BUILD_ISO_WORKDIR
